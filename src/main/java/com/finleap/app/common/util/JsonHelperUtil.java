29d0cf4831b454eca41a4e874d8199cad7789c80
vivek mankonda <vivek@day1tech.com>
2022-04-01 01:44:47 +0530

    Code Cleanup

--- src/main/java/com/finleap/app/common/util/JsonHelperUtil.java
+++ src/main/java/com/finleap/app/common/util/JsonHelperUtil.java
@@ -3,22 +3,10 @@
  */
 package com.finleap.app.common.util;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.util.FileCopyUtils;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import lombok.extern.slf4j.Slf4j;
@@ -81,60 +69,6 @@ public class JsonHelperUtil {
 		log.info(CommonConstants.LOG.EXIT, "getMapper", JsonHelperUtil.class.getSimpleName());
 
 		return mapper;
-	}
-
-	/**
-	 * Get the list of objects from the string data
-	 * 
-	 * @param <T>
-	 * @param data
-	 * @param clazz
-	 * @return
-	 * @throws IOException
-	 */
-	public static <T> List<T> getListOfObjects(String data, Class<T> clazz) throws IOException {
-
-		log.info(CommonConstants.LOG.ENTRY, "getListOfObjects", JsonHelperUtil.class.getSimpleName());
-
-		ObjectMapper mapper = getMapper();
-		CollectionType listType = mapper.getTypeFactory().constructCollectionType(ArrayList.class, clazz);
-
-		log.info(CommonConstants.LOG.EXIT, "getListOfObjects", JsonHelperUtil.class.getSimpleName());
-
-		return mapper.readValue(data, listType);
-	}
-
-	/**
-	 * Extract JSON from file
-	 * 
-	 * @return
-	 * @throws IOException
-	 */
-	public static String getJsonFromFile(String filePath, String[] activeProfiles) throws IOException {
-
-		log.info(CommonConstants.LOG.ENTRY, "getJsonFromFile", JsonHelperUtil.class.getSimpleName());
-
-		String resultJson = "";
-
-		log.info("Active Profile for File Loading Purposes: " + Arrays.toString(activeProfiles));
-
-		if (Arrays.asList(activeProfiles).contains("loc")) {
-
-			File file = new ClassPathResource(filePath).getFile();
-
-			resultJson = new String(Files.readAllBytes(file.toPath()));
-		} else {
-
-			ClassPathResource classPathResource = new ClassPathResource(filePath);
-
-			byte[] binaryData = FileCopyUtils.copyToByteArray(classPathResource.getInputStream());
-
-			resultJson = new String(binaryData, StandardCharsets.UTF_8);
-		}
-
-		log.info(CommonConstants.LOG.EXIT, "getJsonFromFile", JsonHelperUtil.class.getSimpleName());
-
-		return resultJson;
 	}
 
 	/**
